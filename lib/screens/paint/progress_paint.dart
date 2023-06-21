02e1e35b514ea23960bf467e70c3be004301eb2e
TridipBhowmik <tridip.softbdltd@gmail.com>
2023-06-21 18:26:18 +0600

    gg

--- lib/screens/paint/progress_paint.dart
+++ lib/screens/paint/progress_paint.dart
@@ -47,16 +47,19 @@ class ProgressPaint extends CustomPainter {
     // canvas.drawArc(Rect.fromCircle(center: center, radius: radius), startAngle+0.4, sweepAngle+1, false, main);
     // canvas.drawArc(Rect.fromCircle(center: center, radius: radius), startAngle+0.4, 0.01, false, whiteIndicator);
 
-    final linePaint=Paint()..color=Colors.yellow..strokeWidth=10..strokeCap=StrokeCap.round;
-    
-    final rectanglePaint=Paint()..strokeCap=StrokeCap.round..strokeWidth=10..color=Colors.red..style=PaintingStyle.stroke;
-    
-    canvas.drawLine(Offset(size.width*0.1, size.height*0.2), Offset(size.width*0.9, size.height*0.2), linePaint);
+    // final linePaint=Paint()..color=Colors.yellow..strokeWidth=10..strokeCap=StrokeCap.round;
+    //
+    // final rectanglePaint=Paint()..strokeCap=StrokeCap.round..strokeWidth=10..color=Colors.red..style=PaintingStyle.stroke;
+    //
+    // //canvas.drawLine(Offset(size.width*0.1, size.height*0.2), Offset(size.width*0.9, size.height*0.2), linePaint);
+    //
+    // final rect=Rect.fromPoints(Offset(size.width*0.3, size.height*0.3), Offset(size.width*0.7, size.height*0.5));
+    // //canvas.drawRect(rect, rectanglePaint);
 
-    final rect=Rect.fromPoints(Offset(size.width*0.3, size.height*0.3), Offset(size.width*0.7, size.height*0.5));
-    canvas.drawRect(rect, rectanglePaint);
-
-
+    final circlePain=Paint()..color=Colors.green..strokeCap=StrokeCap.round..strokeWidth=20..style=PaintingStyle.stroke;
+    final pinkPaint=Paint()..color=Colors.red..strokeCap=StrokeCap.round..strokeWidth=20..style=PaintingStyle.stroke;
+    canvas.drawCircle(Offset(size.width/2, size.height/2), 100, circlePain);
+    canvas.drawCircle(Offset(size.width/2, size.height/2), 100, pinkPaint);
   }
 
   @override
